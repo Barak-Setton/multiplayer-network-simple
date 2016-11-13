b4ab7169bd354213e06ab2335cdba0ef16a04d35
Barak-Setton <setton.b@gmail.com>
2016-11-14 01:25:45 +0200

    commit 1

---
+++ Assets/Choose.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Choose : MonoBehaviour {
+
+    int choice;
+
+    public GameObject netManager;
+
+    public void onCube()
+    {
+        choice = 0;
+        netManager.GetComponent<GameManagerScript>().btn1();
+    }
+    public void onSphere()
+    {
+        choice = 1;
+        netManager.GetComponent<GameManagerScript>().btn2();
+    }
+
+    public void printChoice()
+    {
+        print(choice);
+    }
+}
